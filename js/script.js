f90e33653db5df02643b90d89002279fea81b623
gryfklapryd <greyfikal9@gmail.com>
2020-05-20 15:58:11 +0700

    creating parralax for jumbotron content

--- js/script.js
+++ js/script.js
@@ -10,3 +10,20 @@ $(".page-scroll").click(function (e) {
 
     e.preventDefault();
 });
+
+$(window).scroll(function () {
+    var wScroll = $(this).scrollTop();
+
+    $('.jumbotron img').css({
+        'transform': 'translate(0px, ' + wScroll / 4 + '%)'
+    });
+
+    $('.jumbotron h1').css({
+        'transform': 'translate(0px, ' + wScroll / 2 + '%)'
+    });
+
+    $('.jumbotron p').css({
+        'transform': 'translate(0px, ' + wScroll / 1.2 + '%)'
+    });
+
+});
